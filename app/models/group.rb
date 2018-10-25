d53d793731f61101503288168331a86e6838087c
Manami0623 <manami.eikaiwa1b@gmail.com>
2018-10-25 19:50:18 +0900

    Chat-space送信機能完了

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,22 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   validates :name, presence: true
   has_many :messages
+
+  validates :name, presence: true
+
+    def show_last_message
+      if (last_message = messages.last).present?
+        last_message.content? ? last_message.content : '画像の投稿があります'
+      else
+        'まだメッセージがありません。'
+      end
+
+    # def show_member
+    #    if (members = groups.name).present?
+    #    members.content ? ?members.content
+    #     else
+    #   'まだメンバーはいません。'
+    #   end
+    # end
+  end
 end
