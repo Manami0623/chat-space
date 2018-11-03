4d187b945c3efe79585d0bd5abb85e451f34e495
Manami0623 <manami.eikaiwa1b@gmail.com>
2018-11-03 17:15:17 +0900

    自動更新（残りはID付与のみ）
    2回目更新されないのでそこまで

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -4,7 +4,8 @@ $(document).on('turbolinks:load', function() {
 
     var html =
     `
-    <div class="message">
+    <div class="message" >
+    <div id =data-id:#{message.id}>
       <div class="line">
         <div class="line__uppername">
           ${message.user_name}
@@ -16,15 +17,39 @@ $(document).on('turbolinks:load', function() {
         <div class="line__centermessage">
             ${message.content}
             ${picture}
-
         </div>
       </div>
     </div>    `
 
-
     return html;
 
   }
+    var interval = setInterval(function() {
+      // if (window.location.pathname.match(/\/groups\/\d+\/messages/)) {
+        var message_id = $('.message:last').data('id'); //一番最後にある'messages'というクラスの'id'というデータ属性を取得し、'message_id'という変数に代入
+        $.ajax({ //ajax通信で以下のことを行う
+          url: location.href, //urlは現在のページを指定
+          type: 'GET', //メソッドを指定
+          data://railsに引き渡すデータは
+            { id: message_id }, //このような形(paramsの形をしています)で、'id'には'message_id'を入れる
+          dataType: 'json' //データはjson形式
+        })
+        .done(function(data){
+          console.log(data);
+          if (data.length !== 0) {
+            var html = '';
+            data.forEach(function(message) {
+              html = buildHTML(message);
+              $('.messages').append(html)
+              $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+            });
+          }
+        });
+      // } else {
+      //   clearInterval(interval);
+      // }
+    }, 5000 );
+
     $('#msg_form').on('submit', function(e){
       e.preventDefault();
       var formData = new FormData(this);
@@ -42,7 +67,7 @@ $(document).on('turbolinks:load', function() {
       var html = buildHTML(data);
       $('.messages').append(html)
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      $('.form__message').reset();
+      $('.form__message').val("");
       $('.form__submit').prop('disabled', false);
 
     })
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,11 @@ class MessagesController < ApplicationController
     @message = Message.new
     @messages = @group.messages.includes(:user)
 
-  end
+    respond_to do |format|
+      format.html
+      format.json { @messages = @messages.where('id > ?', params[:id]) }
+    end
+end
 
   def create
     @message = @group.messages.new(message_params)
